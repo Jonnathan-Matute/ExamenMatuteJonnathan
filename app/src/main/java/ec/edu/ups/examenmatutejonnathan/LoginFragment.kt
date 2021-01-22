6fdfc75a6d859c66ab6c6fd9ce5314e77f443a4e
Jonnathan Oswaldo Matute Curillo <34014602+Jonnathan-Matute@users.noreply.github.com>
2021-01-22 12:34:40 -0500

    Fin
    Fin

--- app/src/main/java/ec/edu/ups/examenmatutejonnathan/LoginFragment.kt
+++ app/src/main/java/ec/edu/ups/examenmatutejonnathan/LoginFragment.kt
@@ -1,10 +1,11 @@
 package ec.edu.ups.examenmatutejonnathan
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ec.edu.ups.examenmatutejonnathan.databinding.FragmentLoginBinding
 
 /**
@@ -20,8 +21,31 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.btnLogin.setOnClickListener{ view: View->
+
+            view.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToInicioFragment())
+        }
+
+        binding.btnRegistrar.setOnClickListener{ view: View->
+
+            view.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegistroFragment())
+        }
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.inicio_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/ec/edu/ups/examenmatutejonnathan/RegistroFragment.kt
+++ app/src/main/java/ec/edu/ups/examenmatutejonnathan/RegistroFragment.kt
@@ -1,10 +1,11 @@
 package ec.edu.ups.examenmatutejonnathan
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ec.edu.ups.examenmatutejonnathan.databinding.FragmentRegistroBinding
 
 /**
@@ -21,6 +22,26 @@ class RegistroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_registro, container, false)
+
+        binding.btnRegister.setOnClickListener{ view: View->
+
+            view.findNavController().navigate(RegistroFragmentDirections.actionRegistroFragmentToLoginFragment())
+        }
+
+
+        setHasOptionsMenu(true)
+
+        return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.inicio_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+
 }
